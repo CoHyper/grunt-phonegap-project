8563c16b919c846bd45aae5449bbf9557275b10b
svenlang <svenlang07@gmail.com>
2014-05-24 11:28:45 +0200

    test // add todo

--- test/phonegap_project_test.js
+++ test/phonegap_project_test.js
@@ -26,6 +26,16 @@ exports.phonegap_project = {
     setUp: function(done) {
     // setup here if necessary
         done();
+    },
+    default_options: function(test) {
+        // TODO
+        test.expect(1);
+
+        var actual = true;
+        var expected = true;
+        test.equal(actual, expected, 'should describe what the default behavior is.');
+
+        test.done();
     }
 /*
 
