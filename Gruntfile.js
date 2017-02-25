cb0f6cae0dc87ac6988ecdad579cc37d4e2fe237
SvenLang <SvenLang07@gmail.com>
2017-02-25 17:57:12 +0100

    move tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,122 +2,95 @@
  * grunt-phonegap-project
  * https://github.com/CoHyper/grunt-phonegap-project
  *
- * Copyright (c) 2014 svenlang
+ * Copyright (c) 2014 SvenLang
  * Licensed under the MIT license.
  */
+
 'use strict';
 
-var _ = require('lodash');
+module.exports = function (grunt) {
 
-module.exports = function(grunt) {
-    grunt = _.isObject(grunt) ? grunt : {};
+	// Project configuration.
+	grunt.initConfig({
 
-    /**
-     * Testing local and with Travis without any changes
-     * TODO : perhaps other solution
-     *
-     * @method getTaskValues
-     * @returns {Object}
-     */
-    function getTaskValues() {
+		// Configuration to be run (and then tested).
+		phonegap_project: {
+			test_local: {
+				options: {
+					path: "build/test_only_platforms",
+					platforms: [
+						"browser",
+						"android",
+						"ios"
+					],
+					plugins: [
+						"cordova-plugin-battery-status",
+						"cordova-plugin-camera"
+					]
+				}
+			},
+			travis_nothing: {
+				options: {
+					path: "build/travis_nothing"
+				}
+			},
+			travis_only_platforms: {
+				options: {
+					path: "build/travis_only_platforms",
+					platforms: [
+						"browser",
+						"android"
+					]
+				}
+			},
+			travis_only_plugins: {
+				options: {
+					path: "build/travis_only_plugins",
+					plugins: [
+						"cordova-plugin-battery-status",
+						"cordova-plugin-camera"
+					]
+				}
+			},
+			travis_all: {
+				options: {
+					path: "build/travis_all",
+					platforms: [
+						"browser"
+					],
+					plugins: [
+						"cordova-plugin-battery-status",
+						"cordova-plugin-camera",
+						"cordova-plugin-console",
+						"cordova-plugin-contacts",
+						"cordova-plugin-device",
+						"cordova-plugin-device-motion",
+						"cordova-plugin-device-orientation",
+						"cordova-plugin-dialogs",
+						"cordova-plugin-file",
+						"cordova-plugin-file-transfer",
+						"cordova-plugin-geolocation",
+						"cordova-plugin-globalization",
+						"cordova-plugin-inappbrowser",
+						"cordova-plugin-media",
+						"cordova-plugin-media-capture",
+						"cordova-plugin-network-information",
+						"cordova-plugin-splashscreen",
+						"cordova-plugin-statusbar",
+						"cordova-plugin-vibration",
+						"cordova-plugin-whitelist"
+					]
+				}
+			}
+		}
 
-        // testing with Travis - add all cordova plugins, but no supported multilanguage (cant install iOS and/or Android SDK)
-        var jsonFile = 'tasks/options/options_production.json',
-            obj;
+	});
 
-        // check if file local exists
-        if (grunt.file.isFile('../grunt_dummy.js')) {
-            grunt.log.ok('########################');
-            grunt.log.ok('# STATUS "DEVELOPMENT" #');
-            grunt.log.ok('########################');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-            // local test with own file
-            jsonFile = 'tasks/options/options_development.json';
-        }
-        obj = grunt.file.readJSON(jsonFile);
 
-        return _.isObject(obj) ? obj : {};
-    }
-
-    // Project configuration.
-    grunt.initConfig({
-
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js'
-            ],
-            options: {
-                "curly": true,
-                "eqeqeq": true,
-                "immed": true,
-                "latedef": true,
-                "newcap": true,
-                "noarg": true,
-                "sub": true,
-                "undef": true,
-                "boss": true,
-                "eqnull": true,
-                "node": true
-            }
-        },
-
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: [
-                'tmp',
-                // default folder
-                'phoneGapProject',
-                // settings folder
-                'newapp',
-                // default folder
-                'phoneGapProject',
-
-                // settings folder
-                'myyApp'
-            ]
-        },
-
-        // Configuration to be run
-        phonegap_project: {
-            options: {
-                //path: 'newapp',
-                androidMinSdk: 20,
-                androidTargetSdk: 30,
-                version: "2.3.4",
-                copyConfigXml: true
-            },
-            create: {
-                title: 'NewApp',
-                bundleId: 'de.myylinks.newapp',
-                platforms: getTaskValues().platforms,
-                plugins: getTaskValues().plugins,
-                deleteOptionsPath: true,
-                access: [
-                    "http://myylinks.de",
-                    "http://www.myylinks.de"
-                ]
-            },
-            build: {
-                platforms: getTaskValues().platforms
-            }
-        }
-
-    });
-
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
-
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-
-    // By default, jshint and create new project.
-    grunt.registerTask('default', ['clean', 'jshint', 'phonegap_project']);
-    grunt.registerTask('clean files', ['clean']);
-
-    // All "phonegap_project" tasks
-    grunt.registerTask('1 create new App', ['phonegap_project:create']);
-    grunt.registerTask('2 build App', ['phonegap_project:build']);
+	// By default run all tests.
+	grunt.registerTask('default', ['phonegap_project']);
 
 };
